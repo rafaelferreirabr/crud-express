00f862c6270ba4383310fa3972ee1ac957190713
rafaelferreirabr <rafamarques03@gmail.com>
2020-07-11 22:22:06 -0300

    Configure prettier and eslint

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express'
+import bodyParser from 'body-parser'
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
-);
-app.get("/", function(req, res) {
-  const hello = JSON.stringify("Hello World!");
-  res.send(hello);
-});
+)
+app.get('/', function (req, res) {
+  const hello = JSON.stringify('Hello World!')
+  res.send(hello)
+})
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
-});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
