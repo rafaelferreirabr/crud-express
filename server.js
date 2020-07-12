f1d36cdf9b29a64e953d194a5db04b42a5238fa2
rafaelferreirabr <rafamarques03@gmail.com>
2020-07-11 22:11:24 -0300

    Fix .gitignore, config nodemon and bodyparser

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.get("/", function(req, res) {
   const hello = JSON.stringify("Hello World!");
   res.send(hello);
